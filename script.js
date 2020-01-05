2b317527e9eddfc0808a256e8fa50894e4d8561e
Maxim Obelets <maximobelets@gmail.com>
2020-01-06 00:09:04 +0500

    added speed of text

--- script.js
+++ script.js
@@ -2,16 +2,15 @@
 
 const text = document.querySelector('.text');
 
-const typeWriter = (string) => {
+const typeWriter = (string, speed) => {
     for(let i = 0; i < string.length; i++ ) {
         setTimeout((i) => {
             text.innerHTML += string[i];
-            console.log(i)
-        }, i*1000, i)
+        }, i * (speed * 1000), i)
     }
-}
+};
 
-typeWriter('text')
+typeWriter('text', 5);
 
 
 
